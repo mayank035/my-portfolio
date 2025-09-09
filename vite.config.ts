1dd24493b410122b9aecf615b156c15d99f48d69
Mayank Srivastava <osndl.mayanks@aicofindia.com>
2025-09-09 16:32:26 +0530

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/my-portfolio/',   // 👈 yeh add karo (repo name ke hisaab se)
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: "/my-portfolio/",   // ⚡ yaha repo ka naam
-})
+});
